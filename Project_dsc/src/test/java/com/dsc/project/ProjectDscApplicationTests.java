4cb2cef78cb089ea4e21de455b541daba0d5e0a4
MuskanAgrawal0405 <95519097+MuskanAgrawal0405@users.noreply.github.com>
2021-12-06 01:07:43 +0530

    Add files via upload

---
+++ Project_dsc/src/test/java/com/dsc/project/ProjectDscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsc.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
